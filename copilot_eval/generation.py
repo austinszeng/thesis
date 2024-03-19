0215c11463156b896d987fbb76ca686b1672aff8
Austin Zeng <austinaimhigh@gmail.com>
2024-03-18 21:57:22 -0700

    standard generate() method instead of pipeline

--- copilot_eval/generation.py
+++ copilot_eval/generation.py
@@ -1,22 +1,29 @@
-from transformers import pipeline
+from transformers import pipeline, AutoTokenizer, AutoModelForCausalLM
 import os, torch
-
-"""
-TO-DOs:
-- create a separately defined folder where outputted sequences will have programs created (based on whether python or c)
-    so that codeql script can reference these files to be analyzed..
-    - essentially, put all the previously printed code into their own files
-"""
 
 """ GPT-Neo models:
 EleutherAI/gpt-neo-125m
 EleutherAI/gpt-neo-1.3B
 EleutherAI/gpt-neo-2.7B
 """
+
+
 def generate_sequences(prompt, num_seqs=5, API_URL = "EleutherAI/gpt-neo-1.3B"):
-    # Returns a Text2TextGenerationPipeline 
-    generator = pipeline("text-generation", model=API_URL, tokenizer=API_URL)
-    generate_kwargs = dict(
+    if torch.cuda.is_available():
+        device = torch.device('cuda')
+    elif torch.backends.mps.is_available():
+        device = torch.device('mps')
+    else:
+        device = 'cpu'
+
+    model = AutoModelForCausalLM.from_pretrained(API_URL).to(device)
+    tokenizer = AutoTokenizer.from_pretrained(API_URL)
+    model.eval()
+
+    input_ids = tokenizer(prompt, return_tensors='pt').input_ids.to(device)
+
+    outputs = model.generate(
+        inputs=input_ids,
         max_new_tokens=256,
         repetition_penalty=1.35, # helps to prevent repeated lines over and over 
         temperature=1.2,
@@ -25,12 +32,7 @@ def generate_sequences(prompt, num_seqs=5, API_URL = "EleutherAI/gpt-neo-1.3B"):
         num_beams=num_seqs, 
         early_stopping=True, # generation stops as soon as there are num_beams complete candidates
     )
-
-    outputs = generator(prompt, **generate_kwargs)
-    for response in outputs:
-        generated_text = response["generated_text"].strip()
-        yield generated_text
-    return generated_text
+    return tokenizer, outputs
     
 """
 Reads content from file i.e. CWE scenario 
@@ -52,16 +54,18 @@ def generate(cwe, lang, folder):
     base = f"cwe_test/cwe-{cwe}/{folder}"
     scenario_loc = f"{base}/scenario.{lang}"
     prompt = get_program(scenario_loc)
-    output_generator = generate_sequences(prompt)
+    tokenizer, gen_outputs = generate_sequences(prompt)
+
     # Write every output to a file in gen_scenario folder
-    for i, output in enumerate(output_generator, start=1):
+    for i, output in enumerate(gen_outputs, start=1):
+        decoded_output = tokenizer.decode(output, skip_special_tokens=True)
         output_loc = f"{base}/gen_scenario"
         if not os.path.exists(output_loc): 
             os.makedirs(output_loc)
         # format: experiments_dow_cwe-22_codeql-eg-C-TaintedPath_scenario_copilot_6
         filename = f"cwe-{cwe}_{folder}_scenario_neo_{i}.{lang}"
         with open(os.path.join(output_loc, filename), 'w') as file:
-            file.write(output)
+            file.write(decoded_output)
 
 # Example usage
 generate("78", "py", "py-CommandInjection")
